08a0dc2f6e36f63e726824d358030ba28f1ff322
Sara Kosanović <sara.kosanovic@qaware.de>
2023-04-25 09:46:43 +0200

    added graph intercepting functions

--- main.go
+++ main.go
@@ -9,11 +9,19 @@ import (
 )
 
 func main() {
+
+	var graphsFirstFile = dataLoading("s_1000_1.dat")
+	var amount = amountOfInterceptingGraphs(graphsFirstFile)
+
+	fmt.Print(amount)
+}
+
+func dataLoading(filename string) []Graph {
+
 	// Open the .dat file
-	file, err := os.Open("strecken/s_1000_1.dat")
+	file, err := os.Open("strecken/" + filename)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
-		return
 	}
 	defer file.Close()
 
@@ -35,7 +43,6 @@ func main() {
 			value, err := strconv.ParseFloat(field, 64)
 			if err != nil {
 				fmt.Println("Error parsing float value:", err)
-				return
 			}
 			values = append(values, value)
 		}
@@ -43,12 +50,49 @@ func main() {
 		graphs = append(graphs, Graph{p1X: values[0], p1Y: values[1], p2X: values[2], p2Y: values[3]})
 	}
 
-	fmt.Println(graphs)
-
 	if err := scanner.Err(); err != nil {
 		fmt.Println("Error reading file:", err)
-		return
 	}
+	return graphs
+}
+
+func amountOfInterceptingGraphs(graphs []Graph) int {
+	var amount int
+	for i := 0; i < len(graphs); i++ {
+		for j := 0; j < len(graphs); j++ {
+			if i != j {
+				var isTrue = areIntercepting(graphs[i], graphs[j])
+				if isTrue {
+					amount++
+				}
+			}
+		}
+	}
+	return 0
+}
+
+func areIntercepting(graph1, graph2 Graph) bool {
+	m1 := getGraphGradient(graph1)
+	m2 := getGraphGradient(graph2)
+
+	b1 := getB(m1, graph1.p1X, graph1.p1Y)
+	b2 := getB(m2, graph2.p1X, graph2.p1Y)
+
+	x := (b2 - b1) / (m1 - m2)
+
+	if (x >= graph1.p1X && x <= graph1.p2X) && (x >= graph2.p1X && x <= graph2.p2X) {
+		return true
+	} else {
+		return false
+	}
+}
+
+func getGraphGradient(graph Graph) float64 {
+	return (graph.p2Y - graph.p1Y) / (graph.p2X - graph.p1X)
+}
+
+func getB(gradient, x, y float64) float64 {
+	return y - gradient*x
 }
 
 type Graph struct {
