11a3ed8d6c2eee8d2edb2053eff006a0c3ce4069
Sara Kosanović <sara.kosanovic@qaware.de>
2023-04-25 09:15:57 +0200

    added struct Graph and loaded data from file into struct

--- main.go
+++ main.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Open the .dat file
+	file, err := os.Open("strecken/s_1000_1.dat")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	// Create a scanner to read the file
+	scanner := bufio.NewScanner(file)
+
+	var graphs []Graph
+
+	// Loop through each line in the file
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Split the line into fields
+		fields := strings.Fields(line)
+
+		// Convert the fields to float64 values
+		var values []float64
+		for _, field := range fields {
+			value, err := strconv.ParseFloat(field, 64)
+			if err != nil {
+				fmt.Println("Error parsing float value:", err)
+				return
+			}
+			values = append(values, value)
+		}
+
+		graphs = append(graphs, Graph{p1X: values[0], p1Y: values[1], p2X: values[2], p2Y: values[3]})
+	}
+
+	fmt.Println(graphs)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading file:", err)
+		return
+	}
+}
+
+type Graph struct {
+	p1X float64
+	p1Y float64
+	p2X float64
+	p2Y float64
 }
